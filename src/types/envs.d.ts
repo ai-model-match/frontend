5b602679f8c0f8e6b412e0cea8b007fc190d7436
AI Model Match <lory.castelli+modelmatch@gmail.com>
2025-09-23 18:35:49 +0200

    chore: move types declaration in dedicated folder

--- src/types/envs.d.ts
+++ src/types/envs.d.ts
@@ -1,12 +1,3 @@
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
   readonly VITE_DATE_FORMAT: string;
